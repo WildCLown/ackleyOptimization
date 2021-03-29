5424d7a286e8a83848649c81af5e50ddb1fa5f4d
WildClown <gtsa@cin.ufpe.br>
2021-03-28 21:49:59 -0300

    Added function generatePopulation, to create a population of x.

--- ackleyOpt.js
+++ ackleyOpt.js
@@ -2,6 +2,9 @@ const a = 20;
 const b = 0.2;
 const c = 2 * Math.PI;
 const d = 30;
+function print(x){
+    console.log(x);
+}
 function calcAckley(x) {//x é um vetor
     var inF = 0;
     var inS = 0;
@@ -15,11 +18,27 @@ function calcAckley(x) {//x é um vetor
     console.log(result);
     return result;
 }
-function makeX(){
-    var fon = [];
-    for(var i = 0; i<d;i++){
-        fon.push(0);
-    }
-    return fon;
+function generateNumber(min,max){
+    var gen = Math.random() * (max - min + 1) + min
+    //gen = gen.toFixed(4);
+    return gen;
 }
-calcAckley(makeX())
+function generatePopulation(size){
+    var population = [];
+    for(var j = 0; j<size;j++){
+        var x = [];
+        for(var i = 0; i<d;i++){
+            x.push(generateNumber(-15,14));
+        }
+        population.push(x);
+    }
+    //console.log(x);
+    return population;
+}
+
+
+function main(){
+    var population = generatePopulation(10);
+    //console.log(population)
+}
+main();
